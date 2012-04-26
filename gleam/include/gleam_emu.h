c1cbe75fbe0e10b894482d611fed416569d7fa3e
Julian Thatcher <julian@noblesamurai.com>
2012-04-26 23:06:20 +1000

    gleam_emu: Start of implementation

--- gleam/include/gleam_emu.h
+++ gleam/include/gleam_emu.h
@@ -6,26 +6,36 @@
 #define _GLEAM_EMU_H
 
 #include "gleam.h"
+#include "gleam_mem.h"
 
 struct gleam_regs {
 	// Required registers
-	gnum   a0,		// Absolute 0 (offset required to access 0)
-	       cp,		// Code pointer
-		   rp,		// Return pointer
-		   sp,		// Stack pointer
-		   psp,		// Previous stack pointer
-	// Operational registers - these are recalculated at each iteration
-		   eq,		// New value == 0
-		   mt,		// New value > 0
-		   lt,		// New value < 0
-		   ac,		// Accumulator
-		   dc,		// Decumulator
-	// General purpose registers
-		   r0,
-		   r1,
-		   r2,
-		   r3,
-		   r4;
+	gnum a0,		// Absolute 0 (offset required to access 0)
+	     cp,		// Code pointer
+	     rp,		// Return pointer
+	     sp,		// Stack pointer
+	     psp,		// Previous stack pointer
+	     // Operational registers - these are recalculated at each iteration
+	     eq,		// New value == 0
+	     mt,		// New value > 0
+	     lt,		// New value < 0
+	     ac,		// Accumulator
+	     dc,		// Decumulator
+	     // General purpose registers
+	     r0,
+	     r1,
+	     r2,
+	     r3,
+	     r4;
 };
 
+struct gleam_emu {
+	gleam_mem_t *mem;
+};
+typedef struct gleam_emu gleam_emu_t;
+
+gleam_emu_t *emu_new(void);
+gnum emu_init (gleam_emu_t *emu);
+gnum emu_test (gleam_emu_t *emu);
+void emu_free (gleam_emu_t *emu);
 #endif
--- gleam/src/gleam_emu.c
+++ gleam/src/gleam_emu.c
@@ -1,7 +1,54 @@
 /**
  * Gleam emulator
+ *
+ * C implemtation of the Cumulative single-op virtual machine, originally
+ * written in EMCA Script.
+ *
+ * The VM consists of a single operation:
+ *	add *src, inc, *dst
+ *
+ * The add operation takes a source pointer, adds the given number (inc) and
+ * stores the result to the memory dst.
+ *
+ * Registers keep track of various state-related numbers, such as accumulators
+ * and decumulators, as well as maintaining stack pointers.
  */
+
+#include <malloc.h>
+#include <memory.h>
 
 #include "gleam_emu.h"
 #include "gleam_mem.h"
 
+gleam_emu_t *emu_new (void) {
+	gleam_emu_t *emu = (gleam_emu_t*)malloc(sizeof(gleam_emu_t));
+
+		emu->mem = mem_new();
+		if (!emu->mem) {
+			DBG("No mem!\n");
+			free(emu);
+			return 0;
+		}
+
+		if (!mem_test(emu->mem)) {
+			DBG("Mem test failed!\n");
+			mem_free(emu->mem);
+			free(emu);
+			return 0;
+		}
+	return emu;
+}
+
+gnum emu_test (gleam_emu_t *emu) {
+	gnum ret;
+	
+	ret = mem_test(emu->mem);
+	mem_dump(emu->mem);
+
+	return ret;
+}
+
+void emu_free (gleam_emu_t *emu) {
+	mem_free(emu->mem);
+	free(emu);
+}
